909049f689d87484664c66d61ae9663c6a2fa230
Quinade00 <olaleyeadeola27@yahoo.com>
2024-02-25 06:18:16 -0800

    I just added new files

--- 0x1C-makefiles/school.c
+++ 0x1C-makefiles/school.c
@@ -1,10 +1,12 @@
 #include "m.h"
 
 /**
- *  * print_school
+ *  * print_school - print the log of school
  *   *
  *    * Return: Nothing
- *     */
+ *     *
+ *      * Learn more: https://www.alxafrica.com
+ *       */
 void print_school(void)
 {
 		printf("j#0000000000000000000000000000000000000\n");
